77556410e4cd964928fd9215ee6fcb3872fe8d58
Oğuz Bekar <oguzemre.b@gmail.com>
2023-07-22 04:08:34 +0300

    update

--- OnShop/Controllers/HomeController.cs
+++ OnShop/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnShop.Areas.Identity.Data;
 using OnShop.Models;
 using System.Diagnostics;
 
@@ -7,15 +10,21 @@ namespace OnShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly OnShopContext _context;
-        public HomeController(ILogger<HomeController> logger, OnShopContext context)
+        public HomeController(ILogger<HomeController> logger,UserManager<ApplicationUser> userManager, OnShopContext context)
         {
             _logger = logger;
+            this._userManager = userManager;
             _context = context;
+            
         }
 
+        [Authorize]
         public IActionResult Index()
         {
+
+           ViewData["UserID"]= _userManager.GetUserId(this.User);
             return View();
         }
 
